dd1f7ebcd7ef759990fe2e6dd148445952c414be
Rijumone Choudhuri <mailmeonriju@gmail.com>
2016-11-03 16:35:30 +0530

    changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,10 +13,11 @@ angular.module('reportsApp')
 	console.log($scope.startDate);
 	
 	/* fetch report */
-	var input = { "startDate" : "Oct 28 2016 00:00:00 GMT+0530 (India Standard Time)" , "endDate" :"Oct 30 2016 23:59:59 GMT+0530 (India Standard Time)" };
+	var input = { "startDate" : "Oct 28 2016 00:00:00 GMT+0530 (India Standard Time)" , "endDate" :"Oct 28 2016 23:59:59 GMT+0530 (India Standard Time)" };
 	ReportsService.FetchReport(input, function(err, response){
 		if(response){
 			console.log("response", response);
+			
 			$scope.reports = response;
 		}else{
 			console.log("errr", response);
@@ -25,18 +26,49 @@ angular.module('reportsApp')
 
 	$scope.getData = function(){
 
-		console.log($scope.startDate);
-		
+		// console.log($scope.startDate);
+		if(typeof $scope.startDate == "undefined"){$scope.startDate = "Oct 28 2016 00:00:00 GMT+0530 (India Standard Time)";}
+		if(typeof $scope.endDate == "undefined"){$scope.endDate = "Oct 28 2016 23:59:59 GMT+0530 (India Standard Time)";}
 		/* fetch report */
 		var input = { "startDate" : $scope.startDate , "endDate" :$scope.endDate };
+		console.log("input");
+		console.log(input);
 		ReportsService.FetchReport(input, function(err, response){
 			if(response){
-				console.log("response", response);
+				// console.log("response: ", response);
+
 				$scope.reports = response;
 			}else{
-				console.log("errr", response);
+				console.log("error: ", response);
 			}
 		})
 	};
 
-});
+}).directive('exportToCsv',function(){
+  	return {
+    	restrict: 'A',
+    	link: function (scope, element, attrs) {
+    		var el = element[0];
+	        element.bind('click', function(e){
+	        	var table = e.target.nextElementSibling;
+	        	var csvString = '';
+	        	for(var i=0; i<table.rows.length;i++){
+	        		var rowData = table.rows[i].cells;
+	        		for(var j=0; j<rowData.length;j++){
+	        			csvString = csvString + rowData[j].innerHTML + ",";
+	        		}
+	        		csvString = csvString.substring(0,csvString.length - 1);
+	        		csvString = csvString + "\n";
+			    }
+	         	csvString = csvString.substring(0, csvString.length - 1);
+	         	var a = $('<a/>', {
+		            style:'display:none',
+		            href:'data:application/octet-stream;base64,'+btoa(csvString),
+		            download:'report.csv'
+		        }).appendTo('body')
+		        a[0].click()
+		        a.remove();
+	        });
+    	}
+  	}
+	});
--- app/scripts/services/reports.js
+++ app/scripts/services/reports.js
@@ -2,7 +2,7 @@ angular.module('reportsApp')
 .service('ReportsService', function ($q, $http, $rootScope) {
 
     this.FetchReport = function (input, cb) {
-		$http.post('http://analytics.religarehealthinsurance.com/user/path', input)
+		$http.post('http://localhost:1337/user/path', input)
 		.success(function(response){
 			console.log("response", response);
 			return cb(null, response);
@@ -13,3 +13,4 @@ angular.module('reportsApp')
     }
 
 });
+
